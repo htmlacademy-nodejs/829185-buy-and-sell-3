0dc0213c5e7aa6957e1b862b0b64476277ab74ce
unknown <only-666@mail.ru>
2021-04-12 10:52:32 +0300

    module1-task3 добавил файлы

---
+++ src/service/utils.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const shuffle = (someArray) => {
+  for (let i = someArray.length - 1; i > 0; i--) {
+    const randomPosition = Math.floor(Math.random() * i);
+    [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
+  }
+
+  return someArray;
+};
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+module.exports = {
+  shuffle,
+  getRandomInt
+}
