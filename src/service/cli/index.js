c2607d72db069c29444efc33b8d9e98c67872e4d
sergei <only-666@mail.ru>
2021-07-20 11:38:28 +0300

    module5-task2 добавил динамическое добавление данных командой --fill [count]

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -4,11 +4,13 @@ const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
 const server = require(`./server`);
+const fill = require(`./fill`);
 
 const Cli = {
   [generate.name]: generate,
   [help.name]: help,
   [version.name]: version,
+  [fill.name]: fill,
   [server.name]: server
 };
 
